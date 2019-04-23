78e77e6ef5e78ab97b8244875947bcf2f64df671
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-04-23 13:15:29 -0500

    Objectifying algorithm

--- InitialCost/Program.cs
+++ InitialCost/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace InitialCost
 {
-    class Program
+    class Cost
     {
+        public double mealCost = 12.00;
+        public int tipPercent = 20;
+        public int taxPercent = 8;
+
         static void Main(string[] args)
         {
+            double mealCost = Convert.ToDouble(Console.ReadLine());
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            double tip = mealCost * tipPercent / 100;
+            double tax = mealCost * taxPercent / 100;
+            double totalCost = mealCost + tip + tax;
+
+            Console.WriteLine("{0}", Math.Round(totalCost));
+            
+
+
         }
     }
 }
