193690d044c55e3c2b42fa2d1d50da8c771f7102
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-04-23 13:19:59 -0500

    Objectifying solution

--- InitialCost/Program.cs
+++ InitialCost/Program.cs
@@ -12,17 +12,24 @@ namespace InitialCost
         public int tipPercent = 20;
         public int taxPercent = 8;
 
-        static void Main(string[] args)
+        public void TotalCost()
         {
-            double mealCost = Convert.ToDouble(Console.ReadLine());
-            int tipPercent = Convert.ToInt32(Console.ReadLine());
-            int taxPercent = Convert.ToInt32(Console.ReadLine());
-
             double tip = mealCost * tipPercent / 100;
             double tax = mealCost * taxPercent / 100;
             double totalCost = mealCost + tip + tax;
 
             Console.WriteLine("{0}", Math.Round(totalCost));
+        }
+
+        static void Main(string[] args)
+        {
+            Cost myCost = new Cost();
+
+            double mealCost = Convert.ToDouble(Console.ReadLine());
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            myCost.TotalCost();
             
 
 
